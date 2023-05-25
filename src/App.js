9ef21ead294f73f6517f316983d9b3beefb97ab5
primrose <budanaz.yakup@gmail.com>
2023-05-25 20:26:26 +0200

    Initialize tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Bar from './Bar.js';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+    </div>
+    <div className="container mx-auto bg-white-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-500 text-lg">
+        <Bar></Bar>
+      </p>
+    </div>
+    </>
   );
 }
 
---
+++ src/Bar.js
@@ -0,0 +1,28 @@
+const people = [
+    {
+      name: 'Thrud Primrose',
+      email: 'yakup.paradox@gmail.com',
+      role: 'Guy',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+  ]
+  
+  export default function Bar() {
+    return (
+      <ul role="list" className="divide-y divide-gray-100">
+        {people.map((person) => (
+          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            <div className="flex gap-x-4">
+              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+  
