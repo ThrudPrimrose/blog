f2bfa729e5a82a3519475e323e10c23f1a6faa72
thrudprimrose <budanaz.yakup@gmail.com>
2023-11-23 02:52:55 +0100

    Align footer content

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
     return (
         <>
             <footer className="bg-white dark:bg-gray-900 border-2 border-t-gray-200 border-b-white dark:border-b-black">
-                <div className="mx-auto w-full max-w-screen-md">
-                    <div className="px-6 py-2 bg-white dark:bg-gray-700 md:flex md:items-center md:justify-between">
-                        <span className="text-sm text-gray-600 dark:text-gray-300 sm:text-center">© 2023 Yakup Koray Budanaz. All Rights Reserved.
+                <div className="mx-left w-full max-w-screen-md">
+                    <div className="px-6 py-2 bg-white dark:bg-gray-700 md:flex md:items-left md:justify-between">
+                        <span className="text-left text-sm text-gray-600 dark:text-gray-300 sm:text-left">© 2023 Yakup Koray Budanaz. All Rights Reserved.
                             <Link 
                                 href="/disclaimer" to="/disclaimer"
                                 className="underline ml-2"> 
